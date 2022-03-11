1e2f1b1c4c4d5c54b55e7ec3f3ce327b17308fe2
Thomas Athanasiou <55364641+Thomas-Athanasiou@users.noreply.github.com>
2022-03-11 14:47:11 +0200

    @author update

--- Model/ResourceModel/LineItemRejectionReason.php
+++ Model/ResourceModel/LineItemRejectionReason.php
@@ -1,6 +1,6 @@
 <?php
     /**
-     * @author Thomas Athanasiou at Hippiemonkeys
+     * @author Thomas Athanasiou at Hippiemonkeys | @Thomas-Athanasiou
      * @copyright Copyright (c) 2022 Hippiemonkeys (https://hippiemonkeys.com)
      * @package Hippiemonkeys_SkroutzSmartCartWebhook
      */
